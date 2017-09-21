c2e23e6a6b7cc0e27fbba5319e20979ed2660d96
vmornar <vedra@Vedran-Yoga.fer.hr>
2017-09-21 12:51:19 +0200

    Websocket request change

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-var game = require ('./game');
+var game = require('./game');
 var params = require('./cards/params')
 
 const fs = require('fs');
 var options = {
-   key  : fs.readFileSync('/etc/nginx/ssl/nginx.key'),
-   cert : fs.readFileSync('/etc/nginx/ssl/nginx.crt')
+    key: fs.readFileSync('/etc/nginx/ssl/nginx.key'),
+    cert: fs.readFileSync('/etc/nginx/ssl/nginx.crt')
 };
-const https = require ('https');
-var httpsserver = https.createServer (options, app);
+const https = require('https');
+var httpsserver = https.createServer(options, app);
 
-router.use (function(req, res, next) {
-	if (!req.url.startsWith("/cards")) req.url = "/cards" + req.url;
-	//console.log (req.url);
-	next ();
+router.use(function(req, res, next) {
+    if (!req.url.startsWith("/cards")) req.url = "/cards" + req.url;
+    //console.log (req.url);
+    next();
 })
 
 router.get('/cards/deal', function(req, res) {
-    game.deal ();
+    game.deal();
     res.end();
 })
 
@@ -31,7 +31,7 @@ router.get('/cards', function(req, res) {
 })
 
 router.get('/cards/*.gif', function(req, res) {
-    res.sendFile("./static" + req.url.replace ("/cards", ""), {
+    res.sendFile("./static" + req.url.replace("/cards", ""), {
         root: __dirname
     });
 })
@@ -40,62 +40,60 @@ app.use(express.static('cards'));
 app.use(express.static('cards/jquery-ui'));
 app.use(express.static('static'));
 app.use(express.static('.'));
-app.use ('/', router);
+app.use('/', router);
 
 //httpsserver.listen (9002);
 
-var server = httpsserver.listen(params.port, function () {
-  	console.log('http server running at ', server.address().port, ' ', server.address().address);
-	var webSocketServer = new (require('ws')).Server({server:server}),
-	    webSockets = {};
-	console.log('Web Socket server running on same port');
-	
-	webSocketServer.on('connection', function (webSocket) {
-	  console.log("connecting:" + webSocket.upgradeReq.url.substr(1));
-	  var userID = webSocket.upgradeReq.url.substr(1).replace("crdsocket/", "");
-	  webSockets[userID] = webSocket;
-	  console.log("connected:" + userID);
-	
-	  webSocket.on('message', function(message) {
-	    var cmd = JSON.parse(message);
-	    console.log (cmd.command);
-	    if (cmd.command == 'join') {
-	      game.joined (userID);
-	    } else if (cmd.command == 'deal') {
-	      game.deal ();
-	    } else if (cmd.command == 'discard') {
-	      game.discarded (userID, cmd.i);
-	    } else if (cmd.command == 'takeOne') {
-	      game.takeOne (userID);
-	    } else if (cmd.command == 'resetTotals') {
-	      game.resetTotals();
-	    } else if (cmd.command == 'take') {
-	      game.take (userID);
-	    }
-	  });
-	
-	  webSocket.on('close', function () {
-	    delete webSockets[userID];
-	    game.left (userID);
-	    console.log('deleted: ' + userID);
-	  })
-	})
-	
-	exports.webSockets = webSockets;
+var server = httpsserver.listen(params.port, function() {
+    console.log('http server running at ', server.address().port, ' ', server.address().address);
+    var webSocketServer = new(require('ws')).Server({
+            server: server
+        }),
+        webSockets = {};
+    console.log('Web Socket server running on same port');
+
+    webSocketServer.on('connection', function(webSocket, req) {
+        console.log("connecting:" + req.url.substr(1));
+        var userID = req.url.substr(1).replace("crdsocket/", "");
+        webSockets[userID] = webSocket;
+        console.log("connected:" + userID);
+
+        webSocket.on('message', function(message) {
+            var cmd = JSON.parse(message);
+            console.log(cmd.command);
+            if (cmd.command == 'join') {
+                game.joined(userID);
+            } else if (cmd.command == 'deal') {
+                game.deal();
+            } else if (cmd.command == 'discard') {
+                game.discarded(userID, cmd.i);
+            } else if (cmd.command == 'takeOne') {
+                game.takeOne(userID);
+            } else if (cmd.command == 'resetTotals') {
+                game.resetTotals();
+            } else if (cmd.command == 'take') {
+                game.take(userID);
+            }
+        });
+
+        webSocket.on('close', function() {
+            delete webSockets[userID];
+            game.left(userID);
+            console.log('deleted: ' + userID);
+        })
+    })
+
+    exports.webSockets = webSockets;
 });
 
 
 if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ?
+                args[number] :
+                match;
+        });
+    };
 }
-
-
-
